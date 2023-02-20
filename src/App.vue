fdeaa4e8748791301d9c06d3006d0781169941cb
Coco <corentin.recanzone@epitech.eu>
2023-02-20 10:23:27 +0100

    m

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <WindowBar />
-
-  <Header />
-
   <RouterView />
-
-  <Footer />
 </template>
 
 <script lang="ts" setup>
-  import Nav from './components/BaliseSemantique/Nav.vue';
-  import Header from './components/BaliseSemantique/Header.vue';
-  import Footer from './components/BaliseSemantique/Footer.vue';
-  import WindowBar from "./components/WindowBar.vue";
+
 </script>
 
 <style lang="scss" scoped>
