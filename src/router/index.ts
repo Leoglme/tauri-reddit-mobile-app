1c483d35ef86045316c3dbfe4c6edce4df72c444
Geoffrey <geoffrey.guillouet@epitech.eu>
2023-02-20 10:51:01 +0100

    Create View login with button redirect reddit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Index from '../views/IndexView.vue';
+import Login from '../views/LoginView.vue';
+import AccessToken from '../views/AcessToken.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
             path: '/',
             name: 'index',
             component: Index
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/access-token',
+            name: 'Access Token',
+            component: AccessToken
         },
     ]
 })
